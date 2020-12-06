b3526fca2cdaedcc74dad6b15ebe9a8b3f6a9c77
devopsvj <devopsvj@gmail.com>
2020-12-06 14:59:56 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,12 +13,12 @@ resource "aws_instance" "my-tf-ec2-git1" {
 resource "aws_instance" "my-tf-ec2-git2" {
   ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-  tags = {
-    Name  = "my-tf-ec2-git2"
-    Owner = "devopstf"
-    Dept  = "TF Learning"
-    Source  = "terraform local"
-    created_by = "IaC"
-  }
+#   tags = {
+#     Name  = "my-tf-ec2-git2"
+#     Owner = "devopstf"
+#     Dept  = "TF Learning"
+#     Source  = "terraform local"
+#     created_by = "IaC"
+#   }
 }
 
