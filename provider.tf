58a8bd0fc2d7e2ff59870be0127eb92e8c4478bd
devops vj <devopsvj@gmail.com>
2020-12-05 20:05:27 -0500

    Added provider.tf

--- provider.tf
+++ provider.tf
@@ -11,38 +11,3 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-
-resource "aws_instance" "my-tf-ec2-1" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.nano"
-  tags = {
-    Name     = "my-tf-ec2-1"
-    Owner    = "devopstf"
-    Dept     = "TF Learning"
-    Location = "Remote"
-    Source   = "terraform local"
-    created_by = "IaC"
-  }
-}
-resource "aws_instance" "my-tf-ec2-2" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-  tags = {
-    Name  = "my-tf-ec2-2"
-    Owner = "devopstf"
-    Dept  = "TF Learning"
-    Source  = "terraform local"
-    created_by = "IaC"
-  }
-}
-
-resource "aws_iam_user" mycountindiam {
-  name = "iamuser.${count.index}"
-  count = 2
-  path = "/home/"
-  tags = {
-      Name = "mycountindiam.${count.index}"
-      Dept = "TF Learning"
-      created_by = "IaC"
-  }
-}
