2882624607b7c91f3f68e5ce1ac7964df7f2548e
devopsvj <devopsvj@gmail.com>
2020-12-06 15:06:11 -0500

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,10 +1,10 @@
-resource "aws_iam_user" mycountindiamgit {
-  name = "iamuser.${count.index}"
-  count = 2
-  path = "/home/"
-  tags = {
-      Name = "mycountindiamgit.${count.index}"
-      Dept = "TF Learning"
-      created_by = "IaC"
-  }
-}
+# resource "aws_iam_user" mycountindiamgit {
+#   name = "iamuser.${count.index}"
+#   count = 2
+#   path = "/home/"
+#   tags = {
+#       Name = "mycountindiamgit.${count.index}"
+#       Dept = "TF Learning"
+#       created_by = "IaC"
+#   }
+# }
