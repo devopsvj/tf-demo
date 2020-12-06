4b991605ca8595169160824e01fbc944030ad41e
devops vj <devopsvj@gmail.com>
2020-12-05 20:04:01 -0500

    Added iam.tf

--- iam.tf
+++ iam.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_user" mycountindiam {
+resource "aws_iam_user" mycountindiamgit {
   name = "iamuser.${count.index}"
   count = 2
   path = "/home/"
   tags = {
-      Name = "mycountindiam.${count.index}"
+      Name = "mycountindiamgit.${count.index}"
       Dept = "TF Learning"
       created_by = "IaC"
   }
